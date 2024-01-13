702e84ee60a499d5c3a4fa5fb20a95b0c2f91780
techopus <nbhagat@jacobs-university.de>
2024-01-13 18:37:01 +0100

    add nodes again

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/nitin/catkin_ws/src
+# Build directory: /home/nitin/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot_gazebo")
+subdirs("my_robot_control")
+subdirs("my_robot_color_tracking")
+subdirs("my_robot_description")
---
+++ build/my_robot_color_tracking/CMakeFiles/color_tracking_node.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nitin/catkin_ws/src/my_robot_color_tracking/src/color_tracking_node.cpp" "my_robot_color_tracking/CMakeFiles/color_tracking_node.dir/src/color_tracking_node.cpp.o" "gcc" "my_robot_color_tracking/CMakeFiles/color_tracking_node.dir/src/color_tracking_node.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
